706fa0d4710a76070c80c03f71d326b1c40a728e
tanjim-cringebot <70016958+tanjim-cringebot@users.noreply.github.com>
2022-04-15 22:46:26 +0600

    Update money_donor.php

--- money_donor.php
+++ money_donor.php
@@ -5,6 +5,28 @@
     <title> This is for money chosen donors</title>
   </head>
   <body>
-
+    <form action = "">
+      <div class = "container">
+        <h1> Please insert transaction details </h1>
+        <p> Fill in the boxes to complete </p>
+        </hr>
+        
+        <label for = "amt"><b> Amount </b></label>
+        <input type = "text" placeholder = "Amount" name = "amt" id = "amt" required/>
+      
+        <label for = "trx_id"><b> Transaction ID </b></label>
+        <input type = "text" placeholder = "Transaction ID" name = "trx id" id = "trx id" required/>
+        
+        <label for = "acc_num"><b> Account Number </b></label>
+        <input type = "text" placeholder = "Account Number" name = "acc_num" id = "acc_num" required/>
+        
+        <label for = "u_id"><b> Enter ID </b></label>
+        <input type = "text" placeholder = "Enter ID" name = "u_id" id = "u_id" required/>
+        
+        <label for = "pass"><b> Enter Password </b></label>
+        <input type = "password" placeholder = "Enter Password" name = "pass" id = "pass" required/>
+      
+        <label for = "re_pass"><b> Re-enter Password </b></label>
+        <input type = "password" placeholder = "Re-enter Passowrd" name = "re_pass" id = "re_pass" required/>
   </body>
 </html>
