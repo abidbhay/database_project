dd56e9378704f3032f840ecacf81f21912a8944e
Abid Hossain <93929477+abidxg@users.noreply.github.com>
2022-04-16 00:52:48 +0600

    Add files via upload

---
+++ successfull_payment.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+  ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    Your payment has been succesful! Thank you for the donation. <br> <br>
+    <a href="donor_index.php">Go to homepage</a><br><br>
+  </body>
+</html>
