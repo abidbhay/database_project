482abffefe58ddb3f356fda84c38f7ea8a1c578f
Tanjim Hussain Sajin <70016958+tanjim-cringebot@users.noreply.github.com>
2022-04-15 23:19:57 +0600

    Update product_donor.php

--- product_donor.php
+++ product_donor.php
@@ -5,6 +5,25 @@
     <title></title>
   </head>
   <body>
-     This is for product chosen donors
+     <form action = "">
+       <div class = "container">
+       <h1> Donate Products </h1>
+       <p> Please fill in the boxes to complete donation </p>
+       <hr/>
+       
+       <label for = "type"><b> Product Type </b> </label>
+       <input type="text" placeholder = "Product Type" name = 'type' id = 'type' required/>
+       
+       <label for = "qnty"><b> Product Quantity </b> </label>
+       <input type="text" placeholder = "Product Quantity" name = 'qnty' id = 'qnty' required/>
+       
+       <label for = "address"><b> Address </b> </label>
+       <input type="text" placeholder = "Address" name = 'address' id = 'address' required/>
+       </hr>
+    
+       <button class = "subtmitbtn" type = "submit"> Submit </button>
+    </form>
+    
+  </div> 
   </body>
 </html>
