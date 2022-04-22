fb046583c5e549821920cb25fafff9d33f3e8b99
Abid Hossain <93929477+abidxg@users.noreply.github.com>
2022-04-22 15:51:45 +0600

    Update product_donor.php

--- product_donor.php
+++ product_donor.php
@@ -5,25 +5,25 @@
     <title></title>
   </head>
   <body>
-     <form action = "">
-       <div class = "container">
-       <h1> Donate Products </h1>
-       <p> Please fill in the boxes to complete donation </p>
-       <hr/>
-       
-       <label for = "type"><b> Product Type </b> </label>
-       <input type="text" placeholder = "Product Type" name = 'type' id = 'type' required/>
-       
-       <label for = "qnty"><b> Product Quantity </b> </label>
-       <input type="text" placeholder = "Product Quantity" name = 'qnty' id = 'qnty' required/>
-       
-       <label for = "address"><b> Address </b> </label>
-       <input type="text" placeholder = "Address" name = 'address' id = 'address' required/>
-       </hr>
-    
-       <button class = "subtmitbtn" type = "submit"> Submit </button>
-    </form>
-    
-  </div> 
+     This is for product chosen donors <br> <br>
+     <form method="post">
+
+       Enter ProductType:
+       <input id="text" type="text" name="ptype" > <br><br>
+       Enter Product Quantity:
+       <input id="text" type="text" name="quantity" ><br><br>
+       Enter Shipment Address:
+       <input id="text" type="text" name="address" ><br><br>
+       Enter Receiver ID:
+       <input id="text" type="text" name="id" ><br><br>
+       <label for="utype">Choose your user type:</label>
+
+       <br><br>
+       <input  id="button" type="submit"  value="Confirm Donation"><br><br>
+
+       <a href="donor_index.php"> If done, Go back to homepage </a><br><br>
+
+     </form>
+
   </body>
 </html>
