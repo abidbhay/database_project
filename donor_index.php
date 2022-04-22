d3580846b204bd689ca4cb49a86201d41b354c1c
Abid Hossain <93929477+abidxg@users.noreply.github.com>
2022-04-22 15:18:15 +0600

    Update donor_index.php

--- donor_index.php
+++ donor_index.php
@@ -1,28 +1,14 @@
-
 <?php
 session_start();
 
 	include("connection.php");
 	include("functions.php");
-
 	$user_data = check_login($con);
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		//something was posted
 
-		$dtype = $_POST['dtype'];
-		$user_id =  $user_data['user_id'] ;
 
-			if ($dtype==="money"){
-				header("Location: money_donor.php");
-			}
-			if ($dtype==="product"){
-				header("Location: product_donor.php");
-			}
-			die;
-		}
 
-?>
+	?>
+
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -37,75 +23,38 @@ session_start();
   h1 { text-align: center}
  </style>
 <body>
- <h1> <font size="+2"> Welcome to Easy Charity! </font></H1>
-</body>
+ <h1> <font size="+2"> Receivers that have Requested For donation </font></H1>
+
+	 <style type="text/css">
+	  h1 { text-align: center}
+	 </style>
 
 
+		 <font size="1">Hello <?php echo $user_data['user_name'] ?></font><br>
+		 <br>
+		 <a href="msg_query.php"> Send a message</a> <br><br>
+		 <a href="inbox.php">View inbox</a> <br><br>
 
-			<h1>  <font size="1">Hello <?php echo $user_data['user_name'] ?>, This is your profile. You are  our verified donor.</font>
-</h1>
+				Current list of donation requests by receivers:
+				<br>
+				<?php
+        $ID= $user_data['user_id'];
+				$sql= "SELECT personal_userId, personal_name,personal_amount FROM personal";
+				$res=mysqli_query($con, $sql);
+				echo "<table border='2'>";
+				echo "<tr><td>Requester ID</td><td>Requester Name</td><td> Requested Amount</td></tr>";
+				while ($row= mysqli_fetch_assoc($res)){
 
-			 	<h1>  <font size="2"> Want to make donation? Find all places and people you can donate right here:
+					echo "<tr><td>{$row["personal_userId"]}</td><td>{$row["personal_name"]}</td><td>{$row["personal_amount"]}</td></tr>";
+				}
 
-			 </font>
-					<br>
-  <font size="-1">
-		<style type="text/css">
-	   h1 { text-align: center}
-	  </style>
-		<h1>
+				echo "</table>";
+?>
 
+<br><br>
 
+ <a href="donor_index.php">Go back</a><br>
+	<a href="logout.php">Logout</a><br>
+	 <a href="signup.php">Signup as donor/receiver</a><br>
 
-						<table style="border:1px solid black;margin-left:auto;margin-right:auto;">
-							<tr>
-								<th>  <font size="2"> Receiver Name </font></th>
-								<th>  <font size="2"> Receiver ID </font></th>
-							</tr>
-							<?php
-
-								$con= mysqli_connect("localhost","root","", "charity");
-								$sql= 'SELECT * from users WHERE utype="receiver"';
-								$result= $con->query($sql);
-
-								if ($result->num_rows>0){
-									while($row= $result-> fetch_assoc()){
-									 echo	 "<tr><td>".$row['user_name'] . "</td><td>" .$row['user_id'] . "</td>" ;
-									}
-								}
-								else {
-									echo "No results";
-								}
-
-
-								$con->close();
-							 ?>
-						</table>
-						<form method="post">
-
-
-
-			        <label for="utype">Choose your donation type:</label>
-			        <select name="dtype" id="dtype">
-			            <option value="money">Money (cash)</option>
-			          <option value="product">Item/Product</option>
-
-			        </select>
-
-			        <input  id="button" type="submit"  value="Press to Proceed">
-
-							 <br>
-
-
-
-		         </select>
-
-
-						</font>
-						</h1>
-					<br><br>
-			     <br><br>
-					 <br><br>
-		<a href="logout.php">Logout</a>
-  </body>
-</html>
+	</body>
