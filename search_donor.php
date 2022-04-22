b6b89471f4b10bb1f9b70b92f3e1995bd27091e7
saifMokarrom <46572647+saifMokarrom@users.noreply.github.com>
2022-04-23 01:05:46 +0600

    Add files via upload

---
+++ search_donor.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Redirected to forget password</title>
+  </head>
+  <body>
+
+  <h1> User Found!!</h1>
+
+  </body>
+</html>
---
+++ search_donor_not_found.php
@@ -0,0 +1,23 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="error-template">
+
+                <h2>
+                    Not Found</h2>
+                <div class="error-details">
+                    Sorry, No donar with this name!
+                </div>
+                <div class="error-actions">
+                    <a href="http://localhost/370_project-main/receiver.php" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>
+                        Take Me Home </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
