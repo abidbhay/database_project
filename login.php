e8a045fda9adc8c59b7da02f82635cc1cef2c2e9
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-04-22 21:17:44 +0600

    Update login.php

--- login.php
+++ login.php
@@ -2,55 +2,55 @@
 
 session_start();
 
-  include("connection.php");
-  include("functions.php");
+	include("connection.php");
+	include("functions.php");
 
 
-  if($_SERVER['REQUEST_METHOD'] == "POST")
-  {
-    //something was posted
-    $user_name = $_POST['user_name'];
-    $password = $_POST['password'];
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
 
-    if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-    {
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
 
-      //read from database
-      $query = "select * from users where user_name = '$user_name' limit 1";
-      $result = mysqli_query($con, $query);
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
 
-      if($result)
-      {
-        if($result && mysqli_num_rows($result) > 0)
-        {
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
 
-          $user_data = mysqli_fetch_assoc($result);
+					$user_data = mysqli_fetch_assoc($result);
 
-          if($user_data['password'] === $password)
-          {
+					if($user_data['password'] === $password)
+					{
 
-            $_SESSION['user_id'] = $user_data['user_id'];
-            if($user_data['utype']==='donor'){
-              header("Location: donor_index.php");
-            }
-            if($user_data['utype']==='receiver'){
-              header("Location: receiver.php");
-            }
+						$_SESSION['user_id'] = $user_data['user_id'];
+						if($user_data['utype']==='donor'){
+							header("Location: donor_index.php");
+						}
+						if($user_data['utype']==='receiver'){
+							header("Location: receiver.php");
+						}
 						if($user_data['utype']==='admin'){
 							header("Location: admin_index.php");
 						}
 
-            die;
-          }
-        }
-      }
+						die;
+					}
+				}
+			}
 
-      echo "wrong username or password!";
-    }else
-    {
-      echo "wrong username or password!";
-    }
-  }
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
 
 ?>
 
@@ -58,58 +58,45 @@ session_start();
 <!-- <!DOCTYPE html>
 <html>
 <head>
-  <title>Login</title>
+	<title>Login</title>
 </head>
 <body>
-
-  <style type="text/css">
-
-  #text{
-
-    height: 25px;
-    border-radius: 5px;
-    padding: 4px;
-    border: solid thin #aaa;
-    width: 100%;
-  }
-
-  #button{
-
-    padding: 10px;
-    width: 100px;
-    color: black;
-    background-color: lightblue;
-    border: none;
-  }
-
-  #box{
-
-    background-color: grey;
-    margin: auto;
-    width: 300px;
-    padding: 20px;
-  }
-  body {
-      background-image: url("rural.jpg");
-      background-size: cover;
-      background-color: #cccccc;
-  }
-
-  </style>
-
-  <div id="box">
-
-    <form method="post">
-      <div style="font-size: 20px;margin: 10px;color: white;">Login</div>
-
-      <input id="text" type="text" name="user_name"><br><br>
-      <input id="text" type="password" name="password"><br><br>
-
-      <input id="button" type="submit" value="Login"><br><br>
-
-      <a href="signup.php">Click to Signup</a><br><br>
-    </form>
-  </div>
+	<style type="text/css">
+	#text{
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+	#button{
+		padding: 10px;
+		width: 100px;
+		color: black;
+		background-color: lightblue;
+		border: none;
+	}
+	#box{
+		background-color: grey;
+		margin: auto;
+		width: 300px;
+		padding: 20px;
+	}
+	body {
+			background-image: url("rural.jpg");
+			background-size: cover;
+			background-color: #cccccc;
+	}
+	</style>
+	<div id="box">
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="password" name="password"><br><br>
+			<input id="button" type="submit" value="Login"><br><br>
+			<a href="signup.php">Click to Signup</a><br><br>
+		</form>
+	</div>
 </body>
 </html> -->
 
@@ -128,9 +115,15 @@ session_start();
       crossorigin="anonymous"
     />
 
-    <title>Hello, world!</title>
+    <title>Login</title>
+    <style>
+      .de{
+        text-decoration: none;
+      }
+    </style>
   </head>
   <body>
+    
     <header>
       <!-- Navbar -->
 
@@ -165,22 +158,22 @@ session_start();
               </li>
               <li class="nav-item">
                 <a
-                  class="nav-link active fw-bold text-secondary fs-4 me-3"
-                  href="#aboutme"
+                  class="nav-link active fw-bold text-black fs-4 me-3"
+                  href="#aboutus"
                   >About us</a
                 >
               </li>
               <li class="nav-item">
                 <a
-                  class="nav-link active fw-bold text-secondary fs-4 me-3"
-                  href="#ex"
+                  class="nav-link active fw-bold text-black fs-4 me-3"
+                  href="#services"
                   >Services</a
                 >
               </li>
               <li class="nav-item">
                 <a
-                  class="nav-link active fw-bold text-secondary fs-4 me-3"
-                  href="#project"
+                  class="nav-link active fw-bold text-black fs-4 me-3"
+                  href="#login"
                   >Login</a
                 >
               </li>
@@ -198,7 +191,7 @@ session_start();
       <br />
 
       <!-- background -->
-      <section class="container my-5">
+      <section class="container my-5" id="home">
         <div
           id="carouselExampleInterval"
           class="carousel slide"
@@ -236,28 +229,31 @@ session_start();
         </div>
       </section>
     </header>
-
+    
     <main>
-      <div class="container" id="aboutme">
-        <h1 class="text-center">About Us</h1>
-        <br />
-        <p>
-          Shakti Foundation is a non-government organization committed to the
-          economic and social empowerment of disadvantaged women across
-          Bangladesh. It believes in realizing the potential of women to become
-          strong, independent members of their communities. Shakti began its
-          mission with urban microfinance programs and strategically expanded
-          its service network to reach remote rural areas. Over the years, it
-          has widened its range of development services to include basic health
-          care and education, agro-business growth, solar power, skills training
-          and advocacy. Shakti was founded in April 1992 and now serves almost
-          500,000 households within 54 districts of Bangladesh.
-        </p>
+      <div id="aboutus">
+        <div class="container" >
+          <h1 class="text-center">About Us</h1>
+          <br />
+          <p>
+            
+            EasyCharity is a non-government organization committed to the
+            economic and social empowerment of disadvantaged women across
+            Bangladesh. It believes in realizing the potential of women to become
+            strong, independent members of their communities. Shakti began its
+            mission with urban microfinance programs and strategically expanded
+            its service network to reach remote rural areas. Over the years, it
+            has widened its range of development services to include basic health
+            care and education, agro-business growth, solar power, skills training
+            and advocacy. Shakti was founded in April 1992 and now serves almost
+            500,000 households within 54 districts of Bangladesh.
+          </p>
+        </div>
       </div>
 
       <br />
 
-      <div class="container">
+      <div class="container" id="services">
         <h1 class="text-center">Services</h1>
         <br />
         <div class="row gx-5">
@@ -277,33 +273,39 @@ session_start();
       <br>
 
       <!-- Login -->
-      <div class="container ">
+      <div class="container " id="login">
         <h1 class="text-center">Sign in!!</h1>
         <div class="row bg-info align-items-center rounded-3 py-5">
           <div class="col text-center ">
             Join us now to help to create a better platform <br> to help and get help from people.
           </div>
           <div class="col">
-            <form method="post">
+            <form method='post'>
               <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">User name</label> <br>
-                <input type="text"  id="text" name="user_name">
-
+                <label for="exampleInputEmail1" class="form-label">User name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1"  name="user_name">
+                
               </div>
               <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" id="exampleInputPassword1">
               </div>
-
+              
               <button type="submit" class="btn btn-primary">Login</button>
+              
               <br>
               <br>
+              Don't have an account?
+              <a href="signup.php" class="de text-white">Click to Sign Up</a>
+              <br>
+              <br>
+              Forgot your password?
+              <a href="forget_password.php" class="de text-white">Click here to reset</a>
 
-              <a href="signup.php">Click to Signup</a> <br>
-							<a href="forget_password.php"> You forgot your password? </a>
             </form>
           </div>
       </div>
+      
     </main>
 
     <script
