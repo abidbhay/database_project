78c04a814f15e572a5fac90b1f57c63abd3df794
Md. Anonto Shuvo <anontoshuvo99@gmail.com>
2022-04-22 21:21:15 +0600

    Update money_donor.php

--- money_donor.php
+++ money_donor.php
@@ -41,7 +41,7 @@ session_start();
 
 
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -61,24 +61,6 @@ session_start();
               <th>  <font size="2"> Receiver Name </font></th>
               <th>  <font size="2"> Receiver ID </font></th>
             </tr>
-            <?php
-
-              $con= mysqli_connect("localhost","root","", "charity");
-              $sql= 'SELECT * from users WHERE utype="receiver"';
-              $result= $con->query($sql);
-
-              if ($result->num_rows>0){
-                while($row= $result-> fetch_assoc()){
-                 echo	 "<tr><td>".$row['user_name'] . "</td><td>" .$row['user_id'] . "</td>" ;
-                }
-              }
-              else {
-                echo "No results";
-              }
-
-
-              $con->close();
-             ?>
 
     <form method="post" >
       <div class = "container">
@@ -112,5 +94,165 @@ session_start();
     </form>
 
   </div>
+  </body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <title>Redirected to payment (donor)</title>
+    <style>
+      td{
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse justify-content-end"
+          id="navbarScroll"
+        >
+          <ul
+            class="navbar-nav me-2 my-2 my-lg-0 navbar-nav-scroll"
+            style="--bs-scroll-height: 100px"
+          >
+            <li class="nav-item nav-text">
+              <a href="donor_index.php" class="btn" role="button">Home</a>
+            </li>
+
+            <li class="nav-item nav-text">
+              <a href="msg_query.php" class="btn" role="button">Send Message</a>
+            </li>
+
+						<li class="nav-item nav-text">
+              <a href="inbox.php" class="btn" role="button">Inbox</a>
+            </li>
+            
+            <li class="nav-item nav-text">
+              <a href="donation_history.php" class="btn" role="button"
+                >Donation history</a
+              >
+            </li>
+            <li class="nav-item nav-text">
+              <a href="view_request.php" class="btn" role="button"
+                >Donation Request</a
+              >
+            </li>
+            <li class="nav-item">
+              <a href="logout.php" class="btn btn-warning" role="button"
+                >Log Out</a
+              >
+            </li>
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+    <br>
+    <h1 class="text-center">Check out the donation List to donate</h1> <br>
+    <div class="container">
+      <table class="table table-striped table-info" >
+        <thead>
+          <tr >
+            
+            <th scope="col " class="text-center">Receiver Name</th>
+            <th scope="col " class="text-center">Receiver ID </th>
+            
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="td-text">
+
+            
+            <?php
+
+              $con= mysqli_connect("localhost","root","", "charity");
+              $sql= 'SELECT * from users WHERE utype="receiver"';
+              $result= $con->query($sql);
+
+              if ($result->num_rows>0){
+                while($row= $result-> fetch_assoc()){
+                echo	 "<tr><td>" .$row['user_name'] . "</td><td>" .$row['user_id'] . "</td>" ;
+                }
+              }
+              else {
+                echo "No results";
+              }
+
+
+              $con->close();
+            ?>
+            
+          </tr>
+          
+        </tbody>
+      </table>
+
+    </div>
+    <br>
+      
+      <form method="post" >
+        <div class = "container text-center">
+          <h1> Please insert transaction details to Donate</h1>
+          <p> Fill in the boxes to complete </p>
+          </hr>
+          Enter receiver_id for your donation:
+          <input id="text" type="text" name="receiver_id" > <br><br>
+          Enter receiver user_name:
+          <input id="text" type="text" name="receiver_username" > (Optional) <br><br>
+          <label for = "amt"><b> Amount </b></label>
+          <input type = "text" placeholder = "Amount" name = "amt" id = "amt" required/>
+            <br><br>
+          <label for = "trx_id"><b> Transaction ID </b></label>
+          <input type = "text" placeholder = "Transaction ID" name = "trx_id" id = "trx_id" required/>
+          <br><br>
+          <label for = "acc_num"><b> Account Number </b></label>
+          <input type = "text" placeholder = "Account Number" name = "acc_num" id = "acc_num" required/>
+          <br><br>
+          <label for = "u_id"><b> Enter ID </b></label>
+          <input type = "text" placeholder = "Enter ID" name = "u_id" id = "u_id" required/>
+          <br><br>
+          <label for = "pass"><b> Enter Password </b></label>
+          <input type = "password" placeholder = "Enter Password" name = "pass" id = "pass" required/>
+          <br><br>
+          <label for = "re_pass"><b> Re-enter Password </b></label>
+          <input type = "password" placeholder = "Re-enter Passowrd" name = "re_pass" id = "re_pass" required/>
+        </hr>
+  
+        <button class = "submitbtn" type = "submit"> Submit </button>
+      </form>  
+      <br> <br>
+    <br>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
   </body>
 </html>
